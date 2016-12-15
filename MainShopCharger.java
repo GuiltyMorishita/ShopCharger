a25013b075950899303650c2a1158b5b532d9d25
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-12-15 23:52:06 +0900

    insertedStudentCardフィールドを追加

---
+++ MainShopCharger.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class MainShopCharger {
+  private StudentCard insertedStudentCard;
+
+  public void insertStudentCard(StudentCard studentCard) {
+    this.insertedStudentCard = studentCard;
+	}
+}
