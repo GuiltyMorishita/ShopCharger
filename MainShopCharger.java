9b7c6c4c12023136d1c4a79e3b7e6f94d473a538
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-12-16 00:18:22 +0900

    エラーメッセージを追加

--- MainShopCharger.java
+++ MainShopCharger.java
@@ -1,32 +1,44 @@
 import java.util.*;
 
 public class MainShopCharger {
-  private StudentCard insertedStudentCard;
+    private StudentCard insertedStudentCard;
 
-  public void insertStudentCard(StudentCard studentCard) {
-    this.insertedStudentCard = studentCard;
-	}
-
-  public void chargeMoney(int chargeAmount) {
-    if (this.insertedStudentCard != null) {
-      int currentAmount = this.insertedStudentCard.getAccountBalance();
-      int totalAmount = currentAmount + chargeAmount;
-      this.insertedStudentCard.setAccountBalance(totalAmount);
+    public void insertStudentCard(StudentCard studentCard) {
+        this.insertedStudentCard = studentCard;
     }
-	}
 
-  public void printAccountBalance() {
-    if (this.insertedStudentCard != null) {
-      System.out.format("学生名\t: %s\n残高\t: %s\n",
-                        insertedStudentCard.getStudentName(),
-                        insertedStudentCard.getAccountBalance());
+    public void chargeMoney(int chargeAmount) {
+        if (this.insertedStudentCard != null) {
+            int currentAmount = this.insertedStudentCard.getAccountBalance();
+            int totalAmount = currentAmount + chargeAmount;
+            this.insertedStudentCard.setAccountBalance(totalAmount);
+        }
+        else {
+            System.out.println("学生証が挿入されていません");
+        }
     }
-  }
 
-  public static void main(String[] args) {
-    MainShopCharger charger = new MainShopCharger();
-    StudentCard card = new StudentCard("m133383", "森下");
-    charger.chargeMoney(1000);
-    charger.printAccountBalance();
-	}
+    public void printAccountBalance() {
+        if (this.insertedStudentCard != null) {
+            System.out.format("学生名\t: %s\n残高\t: %s\n",
+            insertedStudentCard.getStudentName(),
+            insertedStudentCard.getAccountBalance());
+        }
+        else {
+            System.out.println("学生証が挿入されていません");
+        }
+    }
+
+    public static void main(String[] args) {
+        MainShopCharger charger = new MainShopCharger();
+        charger.chargeMoney(1000);
+        charger.printAccountBalance();
+
+        StudentCard card = new StudentCard("m133383", "森下");
+        charger.insertStudentCard(card);
+        charger.chargeMoney(1000);
+        charger.printAccountBalance();
+        charger.chargeMoney(1000);
+        charger.printAccountBalance();
+    }
 }
--- StudentCard.java
+++ StudentCard.java
@@ -1,38 +1,38 @@
 import java.util.*;
 
 public class StudentCard {
-	private String studentNumber;
-	private String studentName;
-	private int accountBalance;
-  private static ArrayList<StudentCard> studentCardList_ = new ArrayList<StudentCard>();
+    private String studentNumber;
+    private String studentName;
+    private int accountBalance;
+    private static ArrayList<StudentCard> studentCardList_ = new ArrayList<StudentCard>();
 
-  public StudentCard(String number, String name) {
-    this.studentNumber = number;
-    this.studentName = name;
-    this.accountBalance = 0;
-    studentCardList_.add(this);
-  }
-
-  public String getStudentName() {
-    return this.studentName;
-  }
-
-  public void setAccountBalance(int balance) {
-    this.accountBalance = balance;
-  }
-
-  public int getAccountBalance() {
-    return this.accountBalance;
-  }
-
-  public static void main(String[] args) {
-    StudentCard card1 = new StudentCard("m133383", "森下");
-    card1.setAccountBalance(1000);
-    StudentCard card2 = new StudentCard("m133384", "下森");
-    card2.setAccountBalance(2000);
-
-    for (StudentCard c : StudentCard.studentCardList_) {
-      System.out.println(c.getAccountBalance());
+    public StudentCard(String number, String name) {
+        this.studentNumber = number;
+        this.studentName = name;
+        this.accountBalance = 0;
+        studentCardList_.add(this);
     }
-	}
+
+    public String getStudentName() {
+        return this.studentName;
+    }
+
+    public void setAccountBalance(int balance) {
+        this.accountBalance = balance;
+    }
+
+    public int getAccountBalance() {
+        return this.accountBalance;
+    }
+
+    public static void main(String[] args) {
+        StudentCard card1 = new StudentCard("m133383", "森下");
+        card1.setAccountBalance(1000);
+        StudentCard card2 = new StudentCard("m133384", "下森");
+        card2.setAccountBalance(2000);
+
+        for (StudentCard c : StudentCard.studentCardList_) {
+            System.out.println(c.getAccountBalance());
+        }
+    }
 }
