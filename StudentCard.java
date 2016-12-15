555293e04953ccfb778d55addb9995b95042876d
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-12-15 23:31:41 +0900

    StudentCardクラスを格納するstudentCardListを作成した

--- StudentCard.java
+++ StudentCard.java
@@ -4,11 +4,13 @@ public class StudentCard {
 	private String studentNumber;
 	private String studentName;
 	private int accountBalance;
+  private static ArrayList<StudentCard> studentCardList_ = new ArrayList<StudentCard>();
 
   public StudentCard(String number, String name) {
     this.studentNumber = number;
     this.studentName = name;
     this.accountBalance = 0;
+    studentCardList_.add(this);
   }
 
   public void setAccountBalance(int balance) {
@@ -20,9 +22,13 @@ public class StudentCard {
   }
 
   public static void main(String[] args) {
-    StudentCard card = new StudentCard("m133383", "森下");
-    System.out.println(card.getAccountBalance());
-    card.setAccountBalance(1000);
-    System.out.println(card.getAccountBalance());
+    StudentCard card1 = new StudentCard("m133383", "森下");
+    card1.setAccountBalance(1000);
+    StudentCard card2 = new StudentCard("m133384", "下森");
+    card2.setAccountBalance(2000);
+
+    for (StudentCard c : StudentCard.studentCardList_) {
+      System.out.println(c.getAccountBalance());
+    }
 	}
 }
