970f9cd5b49ec86e4d3d224b4896baec6e1f6bca
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-12-16 00:00:26 +0900

    mainにテストを追加

--- MainShopCharger.java
+++ MainShopCharger.java
@@ -8,14 +8,25 @@ public class MainShopCharger {
 	}
 
   public void chargeMoney(int chargeAmount) {
-    int currentAmount = this.insertedStudentCard.getAccountBalance();
-    int totalAmount = currentAmount + chargeAmount;
-    this.insertedStudentCard.setAccountBalance(totalAmount);
+    if (this.insertedStudentCard != null) {
+      int currentAmount = this.insertedStudentCard.getAccountBalance();
+      int totalAmount = currentAmount + chargeAmount;
+      this.insertedStudentCard.setAccountBalance(totalAmount);
+    }
 	}
 
   public void printAccountBalance() {
-    System.out.format("学生名\t: %s\n残高\t: %s\n",
-                      insertedStudentCard.getStudentName(),
-                      insertedStudentCard.getAccountBalance());
+    if (this.insertedStudentCard != null) {
+      System.out.format("学生名\t: %s\n残高\t: %s\n",
+                        insertedStudentCard.getStudentName(),
+                        insertedStudentCard.getAccountBalance());
+    }
   }
+
+  public static void main(String[] args) {
+    MainShopCharger charger = new MainShopCharger();
+    StudentCard card = new StudentCard("m133383", "森下");
+    charger.chargeMoney(1000);
+    charger.printAccountBalance();
+	}
 }
